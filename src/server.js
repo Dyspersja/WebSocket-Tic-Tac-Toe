a1acc127c3c88718598ca479b895b77117a34bb2
Dyspersja <dyspersjamusic@gmail.com>
2024-06-02 16:35:14 +0200

    Added room creation when enough players in queue

--- src/server.js
+++ src/server.js
@@ -11,6 +11,7 @@ const server = http.createServer(app);
 const io = Server(server);
 
 let queue = [];
+let rooms = new Map();
 
 io.on('connection', (socket) => {
     console.log('new user connected');
@@ -27,6 +28,25 @@ io.on('connection', (socket) => {
 
         queue.push(socket);
         socket.emit('queueStatus', 'You have joined the queue.');
+        
+        if (queue.length >= 2) {
+            let roomId = createRoom();
+            const room = rooms.get(roomId);
+
+            let player1 = queue.shift();
+            let player2 = queue.shift();
+
+            player1.join(roomId);
+            player2.join(roomId);
+
+            player1.roomId = roomId;
+            player2.roomId = roomId;
+
+            room.player1.socket = player1;
+            room.player2.socket = player2;
+
+            // TODO: send start game
+        }
     });
 
     socket.on('leaveQueue', () => {
@@ -46,6 +66,29 @@ io.on('connection', (socket) => {
     
     // socket.on('disconnect', () => {});
 });
+
+function generateId() {
+    return Math.random().toString(16).substring(2, 8);
+}
+
+function createRoom() {
+    const roomId = generateId();
+
+    const room = {
+        player1: { socket: null, side: 'X' },
+        player2: { socket: null, side: 'O' },
+        board: Array(9).fill(''),
+        currentPlayer: 'X',
+        score: {
+            player1: 0,
+            player2: 0,
+            draws: 0
+        }
+    };
+
+    rooms.set(roomId, room);
+    return roomId;
+}
 
 server.listen(port, function() {
     console.log(`Server is listening on port: ${port}`);
