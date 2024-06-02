263aa50c03791ba71206bcd9fed9f6dfa911ee89
Dyspersja <dyspersjamusic@gmail.com>
2024-06-02 18:59:53 +0200

    Added AI menu

--- src/public/script.js
+++ src/public/script.js
@@ -15,4 +15,15 @@ $(document).ready(function() {
         $('#loginModal').hide();
         $('#menu').show();
     });
+
+    $('#playVsAIButton').click(function() {
+        $('#aiMenuModal').show();
+    });
+
+    $(window).click(function(event) {
+        var modal = $('#aiMenuModal');
+        if(event.target === modal[0]) {
+            modal.hide();
+        }
+    });
 });
