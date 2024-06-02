90eaa6f4d80719f6477b1c56b5c3727ad5e51d77
Dyspersja <dyspersjamusic@gmail.com>
2024-06-02 18:37:31 +0200

    Added login to client side

--- src/public/script.js
+++ src/public/script.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
-    const socket = io();
+    let socket = io();
+    let username;
+
+    $('#loginForm').submit(function(event) {
+        event.preventDefault();
+        
+        username = $('#username').val().trim();
+        if (username === "") {
+            alert('Username cannot be empty!');
+            return;
+        }
+        
+        socket.emit('setUsername', username);
+        $('#loginModal').hide();
+    });
 });
