f42f8c18132861116c5d689fdd2623ee313b9050
Dyspersja <dyspersjamusic@gmail.com>
2024-06-02 17:50:27 +0200

    Added game logic

--- src/server.js
+++ src/server.js
@@ -94,6 +94,53 @@ io.on('connection', (socket) => {
             socket.emit('error', 'You are not in any room.');
         }
     });
+
+    socket.on('move', (data) => {
+        let roomId = socket.roomId;
+        if (!roomId) {
+            socket.emit('error', 'You are not in any room.');
+            return;
+        }
+
+        let room = rooms.get(roomId);
+        if (!room) {
+            socket.emit('error', 'This room does not exist.');
+            return;
+        }
+
+        if (room.player1.socket !== socket && room.player2.socket !== socket) {
+            socket.emit('error', 'You are not a player in this room.');
+            return;
+        }
+
+        if (room.currentPlayer !== data.side) {
+            socket.emit('error', 'It is not your turn.');
+            return;
+        }
+
+        if (room.board[data.cell] !== '') {
+            socket.emit('error', 'Cell is already occupied.');
+            return;
+        }
+
+        room.board[data.cell] = data.side;
+        io.to(roomId).emit('updateGame', getRoomResponse(room));
+
+        let winner = checkWinner(room.board);
+        if (winner) {
+            if (room.player1.side === winner) {
+                room.score.player1++;
+            } else {
+                room.score.player2++;
+            }
+            io.to(roomId).emit('gameOver', winner);
+        } else if (checkDraw(room.board)) {
+            room.score.draws++;
+            io.to(roomId).emit('gameOver', 'draw');
+        } else {
+            room.currentPlayer = room.currentPlayer === 'X' ? 'O' : 'X';
+        }
+    });
     
     socket.on('disconnect', () => {
         leaveQueue(socket);
@@ -173,6 +220,27 @@ function getRoomResponse(room) {
         currentPlayer: room.currentPlayer,
         score: room.score
     };
+}
+
+function checkWinner(board) {
+    const winningConditions = [
+        [0, 1, 2], [3, 4, 5], [6, 7, 8], // Rows
+        [0, 3, 6], [1, 4, 7], [2, 5, 8], // Columns
+        [0, 4, 8], [2, 4, 6] // Diagonals
+    ];
+
+    for (let condition of winningConditions) {
+        const [a, b, c] = condition;
+        if (board[a] && board[a] === board[b] && board[b] === board[c]) {
+            return board[a];
+        }
+    }
+
+    return null;
+}
+
+function checkDraw(board) {
+    return board.every(cell => cell !== '');
 }
 
 server.listen(port, function() {
